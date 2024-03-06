15c73de1642ddac46f1df3860baf3f78dfd65879
MyChi <mychikhtn@gmail.com>
2024-03-06 10:17:25 +0700

    write example use basic of reactjs

--- src/App.js
+++ src/App.js
@@ -1,14 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
+  const name = 'Hau';
+  const age = 18;
+  const isMale = true;
+  const student = {
+    name: 'Easy Frontend'
+  };
+
+  const colorList = ['red', 'green', 'blue'];
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reloadv12333.
+          My Chi <p>{name} - {isMale ? 'Male' : 'Female'}</p>
         </p>
+
+        {isMale ? <p>Male</p> : <p>Female</p>}
+
+        {/* Co the dung the div hoac dung React.Fragment */}
+
+        {isMale && (
+          <div>
+            <p>Male 1</p>
+            <p>Male 2</p>
+            <p>Male 3</p>
+          </div>
+        )}
+
+        {isMale && (
+          <React.Fragment>
+            <p>Male 1</p>
+            <p>Male 2</p>
+            <p>Male 3</p>
+          </React.Fragment>
+        )}
+
+        <p>{student.name}</p>
+
+        {/* Lam viec voi cai mang thì phải truyền vào cái key, để nó hiểu mỗi mảng có key duy nhất */}
+        <ul>
+          {colorList.map(color => (
+            <li key={color} style={{color}}>{color}</li>
+          ))}
+        </ul>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
