98fdac40f5ac3edcac0274df9bb8af68c547ea83
MyChi <mychikhtn@gmail.com>
2024-03-20 18:32:56 +0700

    Ho tro nut back tren trinh duyet

--- src/features/Product/pages/ListPage.jsx
+++ src/features/Product/pages/ListPage.jsx
@@ -10,6 +10,7 @@ import FilterViewer from '../components/FilterViewer';
 import { useNavigate } from "react-router-dom";
 import queryString from 'query-string';
 import {  useLocation } from 'react-router-dom';
+import { useMemo } from 'react';
 
 ListPage.propTypes = {
     
@@ -45,7 +46,23 @@ function ListPage(props) {
     console.log('navigate', navigate)
     const location = useLocation();
 
-    const  queryParams = queryString.parse(location.search);
+    const  queryParams = useMemo(() => {
+        // Khi nao location search thay doi thi tui moi di tinh lai object query params nay
+        
+        // parse object ten la params
+        const params = queryString.parse(location.search);
+        // ket qua tra ve true => "true"
+
+        return {
+            ...params,
+
+            _page: Number.parseInt(params._page) || 1, 
+            _limit: Number.parseInt(params._limit) || 9,
+            _sort: params._sort || 'salePrice:ASC',
+            isPromotion: params.isPromotion === 'true',
+            isFreeShip: params.isFreeShip === 'true',
+        }
+    }, [location.search]);
 
     const [productsList, setProductsList] = useState();
     const [pagination, setPagination] = useState({
@@ -61,32 +78,32 @@ function ListPage(props) {
 
     // })
 
-    const [filters, setFilters] = useState(() => ({
-        ...queryParams,
+    // const [filters, setFilters] = useState(() => ({
+    //     ...queryParams,
 
-        _page: Number.parseInt(queryParams._page) || 1, 
-        _limit: Number.parseInt(queryParams._limit) || 9,
-        _sort: queryParams._sort || 'salePrice:ASC',
+    //     _page: Number.parseInt(queryParams._page) || 1, 
+    //     _limit: Number.parseInt(queryParams._limit) || 9,
+    //     _sort: queryParams._sort || 'salePrice:ASC',
 
-    }))
+    // }))
 
 
-    useEffect(() => {
-        // navigate("/new-route", { state: { key: "value" } });
-        // Todo: sync filters to URL
-        // Cu moi lan filters hay history thay doi thi tui se push no
-        navigate({
-            // lay duong dan hien tai
-            pathname: location.pathname,
-            search: queryString.stringify(filters)
-        })
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [filters]);
+    // useEffect(() => {
+    //     // navigate("/new-route", { state: { key: "value" } });
+    //     // Todo: sync filters to URL
+    //     // Cu moi lan filters hay history thay doi thi tui se push no
+    //     navigate({
+    //         // lay duong dan hien tai
+    //         pathname: location.pathname,
+    //         search: queryString.stringify(filters)
+    //     })
+    //     // eslint-disable-next-line react-hooks/exhaustive-deps
+    // }, [filters]);
 
     useEffect(() => {
         (async() => {
             try {
-                const {data, pagination} = await ProductApi.getAll(filters);
+                const {data, pagination} = await ProductApi.getAll(queryParams);
                 
                 setProductsList(data);
                 setPagination(pagination);
@@ -96,45 +113,84 @@ function ListPage(props) {
             }
             setLoading(false);
         })();
-    }, [filters]);
+    }, [queryParams]);
 
     // Khi ma state filters thay doi thi trigger thay doi, no chay lai useEffect nay (goi lai api)
 
     const handlePageChange = (e, page) => {
-        setFilters(prevFilters => (
-            {
-                // giu lai nhung tham so filter truoc do
-                ...prevFilters,
-                // doi cai page sang page moi
-                _page: page
-            }
-        ))
+        // setFilters(prevFilters => (
+        //     {
+        //         // giu lai nhung tham so filter truoc do
+        //         ...prevFilters,
+        //         // doi cai page sang page moi
+        //         _page: page
+        //     }
+        // ))
+
+        const filters = {
+            ...queryParams,
+            _page: page
+        };
+
+        navigate({
+            // lay duong dan hien tai
+            pathname: location.pathname,
+            search: queryString.stringify(filters)
+        });
     };
 
     const handleSortChange = (newValue) => {
-        setFilters(prevFilters => (
-            {
-                // giu lai nhung tham so filter truoc do
-                ...prevFilters,
-                // khi ma co sort thay doi, thì mình đổi lại giá trị sort này = newValue mới
-                _sort: newValue
-            }
-        ))
+        // setFilters(prevFilters => (
+        //     {
+        //         // giu lai nhung tham so filter truoc do
+        //         ...prevFilters,
+        //         // khi ma co sort thay doi, thì mình đổi lại giá trị sort này = newValue mới
+        //         _sort: newValue
+        //     }
+        // ))
+
+        const filters = {
+            ...queryParams,
+            _sort: newValue
+        };
+
+        navigate({
+            // lay duong dan hien tai
+            pathname: location.pathname,
+            search: queryString.stringify(filters)
+        });
     };
 
     const handleFiltersChange = (newFilters) => {
-        setFilters(prevFilters => (
-            {
-                // giu lai nhung tham so filter truoc do
-                ...prevFilters,
-                // Nhieu cai filters thay doi nen lay het newFilters luon (nó kết hợp với tất cả những giá trị filter mới mà thằng con nó bao lên)
-                ...newFilters
-            }
-        ));
+        // setFilters(prevFilters => (
+        //     {
+        //         // giu lai nhung tham so filter truoc do
+        //         ...prevFilters,
+        //         // Nhieu cai filters thay doi nen lay het newFilters luon (nó kết hợp với tất cả những giá trị filter mới mà thằng con nó bao lên)
+        //         ...newFilters
+        //     }
+        // ));
+
+        const filters = {
+            ...queryParams,
+            ...newFilters,
+        };
+
+        navigate({
+            // lay duong dan hien tai
+            pathname: location.pathname,
+            search: queryString.stringify(filters)
+        });
     };
 
     const setNewFilters = (newFilters) => {
-        setFilters(newFilters);
+        // setFilters(newFilters);
+
+        navigate({
+            // lay duong dan hien tai
+            pathname: location.pathname,
+            search: queryString.stringify(newFilters)
+        });
     }
 
     return (
@@ -144,14 +200,14 @@ function ListPage(props) {
                     <Grid container spacing={1}>
                         <Grid item className={classes.left}>
                             <Paper elevation={0}>
-                                <ProductFilters filters={filters} onChange={handleFiltersChange} />
+                                <ProductFilters filters={queryParams} onChange={handleFiltersChange} />
                             </Paper>
                         </Grid>
                         <Grid item className={classes.right}>
                             
                             <Paper elevation={0}>
-                                <ProductSort currentSort={filters._sort} onChange={handleSortChange} />
-                                <FilterViewer filters={filters} onChange={setNewFilters} />
+                                <ProductSort currentSort={queryParams._sort} onChange={handleSortChange} />
+                                <FilterViewer filters={queryParams} onChange={setNewFilters} />
 
                                 {loading ? <ProductSkeletonList length={9}/> : <ProductList data={productsList} />}
 
